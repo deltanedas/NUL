75a6a39a76586e63acc082fd93193e5d204f7b25
DeltaNedas <deltanedas@gmail.com>
2019-12-01 15:16:35 +0000

    fixed it and its a benchmark

--- scripts/spinner.js
+++ scripts/spinner.js
@@ -1,18 +1,13 @@
-var rotatorRegion = Core.atlas.find("spinner-rotator");
+var spinRotation = 0.0;
+var rotatorRegion = Core.atlas.find("ohno-drill-rotator");
 var SpinBlock = extend(Block, {
-	/*update: function(tile){
-		this.spinRotation = (this.spinRotation + this.spinSpeed) % 1;
-	},*/
 	draw: function(tile){
-		this.spinRotation = (this.spinRotation + this.spinSpeed) % 1;
-		Draw.rect(rotatorRegion, tile.drawx(), tile.drawy(), this.spinRotation * 360);
+		spinRotation = (spinRotation + 0.001) % 1;
+		Draw.rect(rotatorRegion, tile.drawx(), tile.drawy(), spinRotation * 360);
 	}
 })
 
 var spinner = new SpinBlock("spinner");
-//spinner.update = true
-spinner.spinRotation = 0;
-spinner.spinSpeed = 0.01;
 spinner.localizedName = "Spinny boi";
 spinner.description = "it spin :O";
 spinner.category = Category.production;
